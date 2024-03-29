bf22bd8b3af3ee4f40e74aa509c8aa8a8ed8e738
Sudipto Baul <sudipto174@gmail.com>
2024-03-29 01:58:26 -0400

    Update utils.py

--- GEP/utils.py
+++ GEP/utils.py
@@ -173,7 +173,7 @@ def get_dataloader(image_set, BATCH_SIZE, gpu='cuda'):
     dataset = transform_dataset(image_set, mn, sd)
     device = get_default_device(gpu)
 
-    return DeviceDataLoader(DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True), device)
+    return DeviceDataLoader(DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, num_workers=4, pin_memory=True), device)
 
 
     
